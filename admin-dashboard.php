09aabd68c4816d363ad5ce2f16052c596ea9469c
BitCyberBox <neweletron@protonmail.com>
2025-05-27 14:33:17 +0300

    Update admin-dashboard.php

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -3,7 +3,7 @@ session_start();
 $username = $_SESSION['user'];
 $password = $_SESSION['pass'];
 if($username != 'admin'){
-  header("Location: index.html");
+  header("Location: /");
 }
 else {
   echo("Here's your flag : bcb{12532ae98a209fcb8e677258429c2b03}");
